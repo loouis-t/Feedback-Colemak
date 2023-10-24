09f9382623141a25ee38509d3d8f0c89e9fc0317
Louis Travaux <louis.trvx@gmail.com>
2023-10-24 08:22:00 +0200

    update (default user): add default user for development

--- src/main/java/com/colemak/feedback/controller/HomeController.java
+++ src/main/java/com/colemak/feedback/controller/HomeController.java
@@ -1,14 +1,32 @@
 package com.colemak.feedback.controller;
 
+import com.colemak.feedback.FeedbackApplication;
+import com.colemak.feedback.model.User;
+import com.colemak.feedback.model.UserRepository;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.security.NoSuchAlgorithmException;
+
 @Controller
 public class HomeController {
+    @Autowired
+    UserRepository userRepository;
+
     @GetMapping("/")
-    public String home(Model model, HttpSession session) {
+    public String home(Model model, HttpSession session) throws NoSuchAlgorithmException {
+
+        // Create a test user (development) : TO DELETE
+        User test = new User();
+        test.setEmail("test");
+        test.setName("test");
+        test.setSurname("test");
+        test.setPassword(FeedbackApplication.hashString("test"));
+        userRepository.save(test);
+
 
         // handle session : display login or logout button
         boolean isLoggedIn = session.getAttribute("user") != null;
