78ac3404e1972a99ac379f50c5e286280da5916e
Louis Travaux <louis.trvx@gmail.com>
2023-10-28 11:42:22 +0200

    fix (settings): added '@Controller' annotation to SettingsController

--- src/main/java/com/colemak/feedback/controller/SettingsController.java
+++ src/main/java/com/colemak/feedback/controller/SettingsController.java
@@ -3,11 +3,13 @@ package com.colemak.feedback.controller;
 import com.colemak.feedback.model.UserRepository;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.security.NoSuchAlgorithmException;
 
+@Controller
 public class SettingsController {
 
     @GetMapping("/settings")
