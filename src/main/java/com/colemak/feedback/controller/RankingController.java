6023c27d6286cc7631778a288a60addc8f56e2cf
leeo-p <portetleo@gmail.com>
2023-11-01 16:45:59 +0100

    add : ranking link on stats page + header and leaderboard in ranking page

--- src/main/java/com/colemak/feedback/controller/RankingController.java
+++ src/main/java/com/colemak/feedback/controller/RankingController.java
@@ -1,11 +1,15 @@
 package com.colemak.feedback.controller;
 
 import ch.qos.logback.core.model.Model;
+import com.colemak.feedback.model.User;
 import com.colemak.feedback.model.UserRepository;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Comparator;
+import java.util.List;
 
 @Controller
 public class RankingController {
