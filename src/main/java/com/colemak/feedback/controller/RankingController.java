b6046353defc0a1afc0d106e9c6861758099e674
leeo-p <portetleo@gmail.com>
2023-11-01 15:59:41 +0100

    add : structure page ranking

---
+++ src/main/java/com/colemak/feedback/controller/RankingController.java
@@ -0,0 +1,21 @@
+package com.colemak.feedback.controller;
+
+import ch.qos.logback.core.model.Model;
+import com.colemak.feedback.model.UserRepository;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RankingController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping("/ranking")
+    public String ranking(Model model, HttpSession session) {
+
+        return "ranking";
+    }
+}
