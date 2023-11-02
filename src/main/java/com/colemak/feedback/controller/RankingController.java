f7369d6587f126f8fc755ea20ac0dd37ae42b5cb
leeo-p <portetleo@gmail.com>
2023-11-02 11:40:26 +0100

    display on leaderboard

--- src/main/java/com/colemak/feedback/controller/RankingController.java
+++ src/main/java/com/colemak/feedback/controller/RankingController.java
@@ -1,6 +1,6 @@
 package com.colemak.feedback.controller;
 
-import ch.qos.logback.core.model.Model;
+import org.springframework.ui.Model;
 import com.colemak.feedback.model.Statistics;
 import com.colemak.feedback.model.User;
 import com.colemak.feedback.model.UserRepository;
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
@@ -21,12 +22,13 @@ public class RankingController {
     @GetMapping("/ranking")
     public String ranking(Model model, HttpSession session) {
 
-        List<User> liste = userRepository.findAll();
-        List<Integer> avgSpeedList = null;
+        List<User> users = userRepository.findAll();
+        List<Integer> avgSpeedList = new ArrayList<>();
+        int iterStat = 0;
         int totalSessions;
         double avgWPM = 0;
 
-        for (User user : liste) {
+        for (User user : users) {
             List<Statistics> stats = user.getStatistics();
             if (stats != null) {
                 totalSessions = 0;
@@ -47,7 +49,9 @@ public class RankingController {
 
         avgSpeedList.sort(Comparator.reverseOrder());
 
+        model.addAttribute("iterStat", iterStat);
         model.addAttribute("avgSpeedList", avgSpeedList);
+        model.addAttribute("users", users);
 
         return "ranking";
     }
